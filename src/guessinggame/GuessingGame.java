ec32ac0bc98f486fbe73f27a79447b9cd4940b34
sagerdmn <30279650+sagerdmn@users.noreply.github.com>
2019-01-21 20:28:30 -0600

    Version 2

--- src/guessinggame/GuessingGame.java
+++ src/guessinggame/GuessingGame.java
@@ -12,13 +12,19 @@ public class GuessingGame {
      */
     public static void main(String[] args) {
         int answer = 4;
+        boolean go = true;
+        
         Scanner input = new Scanner(System.in);
-        System.out.print("Please guess a number 1-10:");
-        int guess = input.nextInt();
-        if(guess==answer){
-            System.out.print("Correct!");
+        while(go){
+            System.out.print("Please guess a number 1-10:");
+            int guess = input.nextInt();
+        
+            if(guess==answer){
+                System.out.println("Correct!");
+            }
+            else{
+                System.out.println("Incorrect!");
+            }
         }
-        else{System.out.print("Incorrect!");}
     }
-    
 }
